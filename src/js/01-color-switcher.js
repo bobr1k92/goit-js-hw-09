f392ef2ac509bc4d69f60d908f587f3046483b7d
Denis Mazurov <19bobrik92@gmail.com>
2022-12-07 15:27:11 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
+
+
+const colorGenerator = {
+
+    intervalId: null,
+    isActive: false,
+    start() {
+
+        if (this.isActive){
+            return;
+        };
+
+        function getRandomHexColor() {
+            return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+          };
+        this.isActive = true;
+
+        this.intervalId = setInterval(() =>{
+                document.body.style.backgroundColor = getRandomHexColor();
+                console.log(getRandomHexColor);
+          }, 1000);
+    },
+    stop() {
+        clearInterval(this.intervalId);
+        this.isActive = false;
+    }
+};
+
+refs.startBtn.addEventListener('click', () => {
+    colorGenerator.start();
+});
+
+refs.stopBtn.addEventListener('click', () => {
+    colorGenerator.stop();
+})
+
 
